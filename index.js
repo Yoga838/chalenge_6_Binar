d9d0c5eeb6ccfcbe5839ea1df8572f9a9c801197
Reckend <Spartancoc04@gmail.com>
2023-11-01 15:01:13 +0700

    add a github action

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(
     extended: true,
   }),
 );
+// Middleware untuk menangani kesalahan jika tidak ada path yang cocok
+app.use((req, res, next) => {
+  res.status(404).json({status:404,message:"Not Found"})
+});
 // app.use('/dokumentasi', swagerUi.serve, swagerUi.setup(swaggerJson))
 app.use('/api/v1', router)
 app.get('/', (req, res) => {
