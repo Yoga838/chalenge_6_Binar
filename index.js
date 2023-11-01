5d1d1d0f031d1adde33d622de67def672160b42f
Reckend <Spartancoc04@gmail.com>
2023-11-01 15:14:47 +0700

    delete handle not found path

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.use(
     extended: true,
   }),
 );
-// Middleware untuk menangani kesalahan jika tidak ada path yang cocok
-app.use((req, res, next) => {
-  res.status(404).json({status:404,message:"Not Found"})
-});
 // app.use('/dokumentasi', swagerUi.serve, swagerUi.setup(swaggerJson))
 app.use('/api/v1', router)
 app.get('/', (req, res) => {
