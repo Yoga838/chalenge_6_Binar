ce0fae2b611755cdc0f5fbe333556a3dcacd00fb
Reckend <Spartancoc04@gmail.com>
2023-11-05 06:34:52 +0700

    fix swagger ui not appear in deployment vercel

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const prisma = require('./libs/prisma')
 const swagerUi = require('swagger-ui-express')
 const swaggerJson = require('./openapi.json')
 
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 app.use(express.json({strict : false}))
 app.use(
   bodyParser.urlencoded({
@@ -15,7 +17,7 @@ app.use(
   }),
 );
 
-app.use('/dokumentasi', swagerUi.serve, swagerUi.setup(swaggerJson))
+app.use('/dokumentasi', swagerUi.serve, swagerUi.setup(swaggerJson,{customCssUrl:CSS_URL}))
 app.use('/api/v1', router)
 
 app.use((req, res, next) => {
